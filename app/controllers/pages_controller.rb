e1b637dfeec4b5652365841929b673e542c69451
beth geast <beth.geast@gmail.com>
2024-04-22 14:05:08 +0100

    change made in config so that pundit now works, and added root_path links to navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  # skip_after_action :verify_policy_scoped
+  # before_action :skip_pundit?
 
   def home
     skip_authorization
   end
 
-  def index
-  end
+  # def index
+  # end
 end
